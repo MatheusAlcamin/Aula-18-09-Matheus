1f477fd8c59e7ec872ce320b99bbacb7efb37f0c
MatheusAlcamin <142093780+MatheusAlcamin@users.noreply.github.com>
2024-09-19 00:18:33 +0000

    First

---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Olá, {nome}! Bem-vindo ao meu projeto C#.");
+        }
+    }
+}
